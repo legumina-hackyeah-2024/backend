2d5e23d7698d529f4c92f2b20e83ac80da4faada
Filip <filip.kostecki00@gmail.com>
2024-09-28 17:10:19 +0200

    hotfix for @marchewkowy_ksiaze

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,59 +1,62 @@
-import {
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 import { decode } from 'jsonwebtoken';
 
+import { IS_PUBLIC_KEY } from '../../common/decorators/public.decorator';
+import { UnauthenticatedError } from '../../common/errors/unauthenticated.error';
 import { UserType } from '../../user/enums/user-type.enum';
-import { IS_PUBLIC_KEY } from 'src/common/decorators/public.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   constructor(private reflector: Reflector) {
     super();
   }
-
-  async canActivate(context: ExecutionContext): Promise<boolean> {
+  async canActivate(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context);
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
+      ctx.getHandler(),
+      ctx.getClass(),
     ]);
 
-    const request = context.switchToHttp().getRequest();
-
+    const { req } = ctx.getContext();
     if (isPublic) {
-      if (request.headers.authorization) {
+      if (req.headers.authorization !== undefined) {
         const { userId, userType } = decode(
-          request.headers.authorization.split(' ')[1],
+          req.headers.authorization.split(' ')[1],
         ) as {
           userId: string;
           userType: UserType;
         };
-        request.user = { id: userId, userType };
+        req.user = { id: userId, userType };
       }
       return true;
     }
-
     try {
-      const canActivateResult = await super.canActivate(context);
-      if (!canActivateResult) {
-        return false;
+      if (await super.canActivate(new ExecutionContextHost([req]))) {
+        if (req.headers.authorization !== undefined) {
+          const { userId, userType } = decode(
+            req.headers.authorization.split(' ')[1],
+          ) as {
+            userId: string;
+            userType: UserType;
+          };
+          req.user = { id: userId, userType };
+        }
+        const userTypes = this.reflector.get<UserType[]>(
+          'userTypes',
+          context.getHandler(),
+        );
+        if (userTypes?.length && !userTypes.includes(req.user.userType)) {
+          return false;
+        }
+        return true;
       }
-
-      const userTypes = this.reflector.get<UserType[]>(
-        'userTypes',
-        context.getHandler(),
-      );
-      if (userTypes?.length && !userTypes.includes(request.user.userType)) {
-        return false;
-      }
-
-      return true;
+      return false;
     } catch (err) {
-      throw new UnauthorizedException('Unauthorized');
+      throw new UnauthenticatedError();
     }
   }
 }
